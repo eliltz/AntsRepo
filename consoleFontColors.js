e4a383a4853193ff0e62990991e0be4274195e43
eliltz <eliltz@gmail.com>
2019-08-18 17:58:52 +0300

    -Added handling hello from client and sending the client an ID, also added testing clients

---
+++ consoleFontColors.js
@@ -0,0 +1,78 @@
+
+var dict = []; 
+
+dict.push({
+    key:   'Reset',
+    value: "\x1b[0m"
+});
+dict.push({
+    key:   'Bright',
+    value: "\x1b[1m"
+});
+dict.push({
+    key:   'Dim',
+    value: "\x1b[2m"
+});
+dict.push({
+    key:   'Underscore',
+    value: "\x1b[4m"
+});
+dict.push({
+    key:   'Blink',
+    value: "\x1b[5m"
+});
+dict.push({
+    key:   'Reverse',
+    value: "\x1b[7m"
+});
+dict.push({
+    key:   'Hidden',
+    value: "\x1b[8m"
+});
+dict.push({
+    key:   'FgBlack',
+    value: "\x1b[30m"
+});
+dict.push({
+    key:   'FgRed',
+    value: "\x1b[31m"
+});
+dict.push({
+    key:   'FgGreen',
+    value: "\x1b[32m"
+});
+dict.push({
+    key:   'FgYellow',
+    value: "\x1b[33m"
+});
+dict.push({
+    key:   'FgBlue',
+    value: "\x1b[34m"
+});
+dict.push({
+    key:   'FgMagenta',
+    value: "\x1b[35m"
+});
+dict.push({
+    key:   'FgCyan',
+    value: "\x1b[37m"
+});
+
+
+BgBlack = "\x1b[40m"
+BgRed = "\x1b[41m"
+BgGreen = "\x1b[42m"
+BgYellow = "\x1b[43m"
+BgBlue = "\x1b[44m"
+BgMagenta = "\x1b[45m"
+BgCyan = "\x1b[46m"
+BgWhite = "\x1b[47m"
+
+function getfontColor(colorName){
+   // console.log(colorName);
+    let colorRepresentation = dict.find(f=> f.key.includes(colorName));
+   // console.log(colorRepresentation)
+return colorRepresentation.value;
+}
+
+module.exports.getfontColor = getfontColor;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const antsCmdRouter = require('./antsCmdRouter.js');
+const fontColor = require('./consoleFontColors');
 var bodyParser = require("body-parser");
 const express = require('express'); 
 const app = express(); 
 const Joi = require('joi');
 var http = require('http');
 var path = require("path");
+var util = require('util');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(expres.json());
@@ -14,47 +16,55 @@ app.use(express.static('pages'));
 
 //require('./app/routes/ants.routes.js')(app);
 
-
-// ===========
-var payloads = [];
-var ant = 1;
-payloads.push({id: ant, x: 2, y: 30, angle: 0, ll:'open', ul:'open', rl:'open', bl:'entry', battery:100, type:'scout'});  //1
-payloads.push({id: ant, x: 2, y: 29, angle: 0, ll:'open', ul:'open', rl:'open', bl:'open', battery:90, type:'scout'});  //2
-payloads.push({id: ant, x: 2, y: 28, angle: 0, ll:'open', ul:'open', rl:'open', bl:'open', battery:80, type:'scout'});  //3
-payloads.push({id: ant, x: 2, y: 27, angle: 90, ll:'open', ul:'open', rl:'open', bl:'open', battery:70, type:'scout'});  //4
-payloads.push({id: ant, x: 2, y: 27, angle: 90, ll:'open', ul:'open', rl:'open', bl:'open', battery:70, type:'scout'});  //5
-payloads.push({id: ant, x: 3, y: 27, angle: 90, ll:'open', ul:'open', rl:'open', bl:'open', battery:60, type:'scout'}); //6
-//payloads.push({id: 2, x: 2, y: 30, angle: 0, ll:constants.MAP_ENUM_OPEN, ul:constants.MAP_ENUM_OPEN, rl:constants.MAP_ENUM_OPEN, bl:constants.MAP_ENUM_ENTRY, battery:100, type:constants.ANT_SCOUT});  //1
-
-const dgram = require('dgram');
-const protobuf = require("protobufjs");
-var init = false;
-var TelemetryMessage;
-initProtoBuf();
-var counter = -1;
 // ===========
 
-
+console.log(fontColor.getfontColor('Reset'));
 
 //var expressWs = require('express-ws')(app,server);
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
 
-var clientsArr=[];
+
+let connectedClientsCounter = 0;
+let antsIdsIndicator = 0;
+//var clientsArr=[];
 //, registrationTime :
 var antsClientsArr = [
-	{id: 1, name: 'ant1', antCommandsArr: [ 'f5','r','f3']
+	{id: 1, name: 'ant1', antCommandsArr: ['f5','r','f3']
 		},
 	{id: 2, name: 'ant2', antCommandsArr: ['f2','l','f4']		
 	}
 ];
 
-var commandsArr = [];
+let antsRobotsArr =[];
 
-//var commandsArrFrom
 
-app.get('/api/ants/', (req, res) => {
-	res.send(antsClientsArr);
+var AntRobot = {    
+    init: function (id, name, wsc, wscIp, wscPort,antCommandsArr) {
+        this.id = id;
+		this.name = name;
+		this.wsc = wsc;
+		this.wscIp = wscIp;
+		this.wscPort = wscPort;
+		this.antCommandsArr = antCommandsArr;
+    },
+    
+    describe: function () {
+        var description = `Description <-*****-> Ant Id: ${this.id}, Ant Name: ${this.name}, Ant IP: ${this.wscIp}, Ant Port: ${this.wscPort}. Ant commands:${this.antCommandsArr}`;
+        return description;
+    }
+};
+
+//var commandsArr = [];
+
+app.get('/api/ants/', (req, res) => {	
+	console.log(antsRobotsArr);
+	let custResponse =[];
+	antsRobotsArr.forEach(e => { 
+		custResponse.push({id :e.id, name: e.name, antCommandsArr: e.antCommandsArr});
+	});		
+	console.log(custResponse);		
+	res.send(custResponse);
 });
 
 app.get('/api/ants/:id', (req, res)=>{
@@ -92,47 +102,67 @@ app.post('/api/ants', (req,res)=> {
 app.put('/api/ants/:id', (req, res) => {
 
 try {
-	const schema = {
-		//name: Joi.string().min(3).required()
-			antCommandsArr : Joi.array().items(Joi.string().regex(/[rsf<>\d]/))
-	};
-	const ant = antsClientsArr.find(a=> a.id === parseInt(req.params.id));
-	if (!ant)
-		res.status(404).send("The ant with the given ID was not found.")		
-	const resultOfJoi = Joi.validate(req.body, schema);
+	// const schema = {
+	// 	//name: Joi.string().min(3).required()
+	// 		antCommandsArr : Joi.array().items(Joi.string().regex(/[rsf<>\d]/))
+	// };
+	// const ant = antsClientsArr.find(a=> a.id === parseInt(req.params.id));
+	// if (!ant)
+	// 	res.status(404).send("The ant with the given ID was not found.")		
+	// const resultOfJoi = Joi.validate(req.body, schema);
 
-	if (resultOfJoi.error) {
-		res.status(400).send(resultOfJoi.error.details[0].message);
-		return;
-	}
+	// if (resultOfJoi.error) {
+	// 	res.status(400).send(resultOfJoi.error.details[0].message);
+	// 	return;
+	// }
 	
-	ant.antCommandsArr = req.body.antCommandsArr;
+	//ant.antCommandsArr = req.body.antCommandsArr;
 
-
-	res.send(ant);
+	let antCommandArrayFromRequest =req.body.antCommandsArr;
+	console.log(`antCommandArrayFromRequest=${antCommandArrayFromRequest}`);
+	let idOfAntToSend = parseInt(req.params.id);//req.params.id;
+	console.log(`idOfAntToSend=${idOfAntToSend}`);
+	//res.send(ant);
 	 
 	//************* For demo purposes 06.08.19 *******************************/
-	let antCmdToSend = ant.antCommandsArr[0];
-	var commandToSendToPhysicalAnt = antsCmdRouter.processCommand(ant.antCommandsArr[0]);
-	console.log(`Will send ${antCmdToSend} to the ant`,)
+	//let antCmdToSend = ant.antCommandsArr[0];
+	//let antCmdToSend = antCommandArrayFromRequest;
+//	var commandToSendToPhysicalAnt = antsCmdRouter.processCommand(ant.antCommandsArr[0]);
+	// var commandToSendToPhysicalAnt = antsCmdRouter.processCommand(antCommandArrayFromRequest);
+	// console.log(`Will send ${commandToSendToPhysicalAnt} to the ant`,)
 	
-	//wss.clients[0].
-	let idOfAntToSend = req.params.id;
+	console.log("--->about to try to send the command: ")
+
 	try {
-		if (clientsArr.length >= idOfAntToSend)
-		{
-			console.log(`Command To Send To Physical Ant: ${commandToSendToPhysicalAnt} to the ant`,)
-			clientsArr[idOfAntToSend-1].send(commandToSendToPhysicalAnt);	
+		printConnectedAnts();
+		const antRobotToSendCommandTo = antsRobotsArr.find(a=> a.id == idOfAntToSend);
+		if (!antRobotToSendCommandTo) {
+			console.warn(`Ant with the given ID (${idOfAntToSend}) was not found and cannot send command to it`);
+			res.status(404).send("The ant with the given ID was not found.");	
 		}
+		else{
+			//console.info(antRobotToSendCommandTo);
+			antRobotToSendCommandTo.antCommandsArr = req.body.antCommandsArr;
+			console.log("--->about to proccess command");
+			var commandToSendToPhysicalAnt = antsCmdRouter.processCommand(antRobotToSendCommandTo.antCommandsArr[0]);
+			console.log(`-->> returned from process command ${commandToSendToPhysicalAnt}`);
+			//res.status(200).send(antRobotToSendCommandTo.antCommandsArr);
+			console.info(`Sending ${commandToSendToPhysicalAnt} command To the physical Ant with the requested id (${idOfAntToSend}) -> ${antRobotToSendCommandTo.describe()}`);
+			antRobotToSendCommandTo.wsc.send(commandToSendToPhysicalAnt);
+		}	
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 
-	
-	//   wss.clients.forEach(function each(client) {
-	// 	  console.log(`Client -> ${client}`);
-	//   });
-	//************************************************************************/
+	// try {
+	// 	if (clientsArr.length >= idOfAntToSend)
+	// 	{
+	// 		console.log(`Command To Send To Physical Ant: ${commandToSendToPhysicalAnt} to the ant`,)
+	// 		clientsArr[idOfAntToSend-1].send(commandToSendToPhysicalAnt);	
+	// 	}
+	// } catch (error) {
+	// 	console.log(error);
+	// }
 		
 } catch (error) {
 		res.status(500).send(error);
@@ -153,29 +183,44 @@ try {
 // });
 
 
-
-//when browser sends get request, send html file to browser. viewed at http://localhost:30000
-app.get('/cmds', function(req, res) {
-	
-	if (!commandsArr) {
-		commandsArr.forEach(element => {
-			console.log(element);				
-		});
-	}
-});
-
-
 function noop() {}
-function heartbeat() {
-  this.isAlive = true;
+class heartbeat {
+	constructor() {
+		this.isAlive = true;
+	}
 }
 
+function getIpv4Adress(address){
 
+	template = /^:(ffff)?:(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+	has_ipv4_version = template.test(address);
+	if (has_ipv4_version)
+		return address;
+	else
+		return address.replace(/^.*:/, '');
 
-wss.on('connection',function(ws,req){
-	clientsArr.push(ws);
+	
+}
+
+wss.on('connection',function(ws,req){	
+	//clientsArr.push(ws);
+	console.log(fontColor.getfontColor("Green"),"");
+	connectedClientsCounter++;
+	antsIdsIndicator++;
+	var tempAnt = Object.create(AntRobot);	
+	tempAnt.init(antsIdsIndicator, `ant ${antsIdsIndicator}`, ws, getIpv4Adress(req.connection.remoteAddress), req.connection._peername.port,['f5','r','f3']);
 	console.log("")
-	console.log(`Number of connected clients: ${wss.clients.size}`);
+	console.log(tempAnt.describe());
+	//Send id to the ant.
+	
+	let connectionObj = req.connection;
+	//console.log(connectionObj);
+	antsRobotsArr.push(tempAnt);
+	//console.log(`Number of connected clients: ${wss.clients.size}`);
+	console.log("New Client is connected.")
+	console.log(`Number of currently connected clients: ${connectedClientsCounter}`);
+	
+	printConnectedAnts();
 	console.log("Either Type Command or get it by the API:")
 	//console.log("f/ b/ r/ l/ </ >/ s--> 0-9")
 	var stdin = process.openStdin();
@@ -186,26 +231,74 @@ wss.on('connection',function(ws,req){
 		console.log("you entered: [" + 
 			d.toString().trim() + "]");
 			var strCmd = d.toString().trim();
+			if (strCmd=="i") {
+				
+			console.log(`---->Sending this ant id to the ant, on connection ${tempAnt.id}`);
+			console.log(`---->AID_${tempAnt.id}`);
+			ws.send(`AID_${tempAnt.id}`);
+			}
+			else
+			{
 			var commandToSend= antsCmdRouter.processCommand(strCmd);
 			console.log(commandToSend);
 		ws.send(commandToSend);
+	}
+		//ws.send(`AID_${tempAnt.id}`);
 	  });
+
+	//   console.log(`---->Sending this ant id to the ant, on connection ${tempAnt.id}`);
+	// console.log(`---->AID_${tempAnt.id}`);
+	// ws.send(`AID_${tempAnt.id}`);
+	  
 	ws.on('message',function(message){
 		//const msg = JSON.parse(message); //TODO: what if the message is not a JSON?...
 		console.log("this is a message from client: " + message);
-		if(message.toLowerCase().includes('finish')) {
-			console.log('sending to server');
-			reportCompletedToUi();
+
+		if (message.includes("Hello Server")) { //send the id on the first message from the client
+			console.log(`---->Sending this ant id to the ant, on connection ${tempAnt.id}`);
+			console.log(`---->AID_${tempAnt.id}`);
+			ws.send(`AID_${tempAnt.id}`);
 		}
-		//ws.send('GF');
-		
-		//ws.send("Server: got message: '" + message + "'"); //send to client where message is from
+	
+		  
+		if(message.toLowerCase().includes('finish')) {
+			console.log('sending to server: ' + message);
+			reportFinish(message);
+		}
+	
 	});
+
+const dgram = require('dgram');
+function reportFinish(message) {
+	let arr = message.split('_');
+	let ant = 1;
+	if((arr[arr.length-1] + '').includes('2')) ant = 2;
+	if((arr[arr.length-1] + '').includes('3')) ant = 3;
+    const client = dgram.createSocket('udp4');
+    client.send('ANTS:FINISH_' + ant, 1337, 'localhost', (err) => {
+        if (err != null) console.log('Err: ' + err);
+            client.close();
+    });
+};
 	
 
-	ws.on('close', function(){
-		//clientsArr.pop(ws);
-		console.log(`lost one client ${getCurrentDate()}`);
+	ws.on('close', function(data,reasonCode, description){	
+		console.log(fontColor.getfontColor("Magenta"),"");
+		connectedClientsCounter--;	
+		console.log(`-----------------------Lost one client -> ${getIpv4Adress(connectionObj.remoteAddress)}::${connectionObj._peername.port}-----------------------${getCurrentDate()}`);
+		//(a.wscIp === connectionObj.remoteAddress &&
+		const antRobotToRemove = antsRobotsArr.find(a=>  a.wscPort === connectionObj._peername.port );
+		if (antRobotToRemove) {
+			console.log("About to remove this ant:");
+			console.log(antRobotToRemove.describe());
+			antsRobotsArr.splice(antsRobotsArr.indexOf(antRobotToRemove),1);
+		}
+		else {
+			console.log("AntRobot to remove not found");
+		}
+		console.log(fontColor.getfontColor('Reset'));		
+		printConnectedAnts();
+		
 	});
 
 	
@@ -215,37 +308,32 @@ wss.on('connection',function(ws,req){
 		//antCommandsArr : req.body.antCommandsArr
 	};
 	
-	ws.send(`Hi new client!, you are client number ${wss.clients.size}`);
+	//ws.send(`Hi new client!, you are client number ${wss.clients.size}`);
+	ws.send(`Hi new client!, you are client number ${connectedClientsCounter}`);
+	// console.log(`---->SENDING AID_${tempAnt.id}`);
+	// ws.send(`AID_${tempAnt.id}`);
+	// ws.send(`AC_${tempAnt.id}`);
 	console.log("");
 	console.log(`New client connected ${getCurrentDate()}`);
+	console.log(fontColor.getfontColor('Reset'));
 });
 
 
 console.log(`Starting server on port 3000 --> ${getCurrentDate()}`);
 server.listen(3000);
-//console.log("Type Command:")
-//console.log("f/ b/ r/ l/ tr/ tl/ s--> 0-9")
-var stdin = process.openStdin();
-stdin.addListener("data", function(d) {
-	console.log("you entered: [" + 
-		d.toString().trim() + "]");
-		var strCmd = d.toString().trim();
-		if	(strCmd == "cmds") 
-		{
-			console.log("list of commands awaiting to be sent:")
-			commandsArr.forEach(function(element, index) {
-			console.log('%d: %s', index +1, element);
-				
-			});
-		}
-		else
-		{
-			var commandToSend= antsCmdRouter.processCommand(strCmd);
-			commandsArr.push(commandToSend);
-			console.log(commandToSend);
-		}	
-  });
 
+function printConnectedAnts() {
+	if (antsRobotsArr.length == 0) {
+		console.log("There are no connected ants at this moment.")
+	}
+	else {
+		console.log("These are the currently connected Ants:");
+		for (let index = 0; index < antsRobotsArr.length; index++) {
+			console.log(`${index + 1}. ${antsRobotsArr[index].describe()}`);
+		}
+	}
+	console.log("");
+}
 
 function getCurrentDate(){
 var today = new Date();
@@ -254,46 +342,3 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 var dateTime = date+' '+time;
 return dateTime
   }
-
-// ===============================
-
-function initProtoBuf(){
-  protobuf.load("D:\\Ants\\realAnt\\AntsRepo\\telemetryMessage.proto", function(err, root) {
-    if (err){
-		console.log(err);
-		throw err;
-	}
-
-	TelemetryMessage = root.lookupType("telemetrypackage.TelemetryMessage");
-	reportCompletedToUi(); // ant start point
-  });
-};
-
-function sendTelemetry(payload){
-	const errMsg = TelemetryMessage.verify(payload);
-	if (errMsg){
-		console.log('Error:' + errMsg);
-		return;
-	}
-	else{
-		const message = TelemetryMessage.create(payload);
-		const buffer = TelemetryMessage.encode(message).finish();
-
-	   const client = dgram.createSocket('udp4');
-	   client.send(buffer, 41848, 'localhost', (err) => {
-		   if (err != null) console.log('Err: ' + err);
-		   client.close();
-	   });
-	}
-}
-
-function reportCompletedToUi(){
-	//if(!init){
-	//	init = true;
-	//	initProtoBuf();
-	//	console.log(TelemetryMessage);
-	//}
-	counter++;
-	console.log(payloads[counter]);
-	sendTelemetry(payloads[counter]);
-}
